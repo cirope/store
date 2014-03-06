f17460226d08ad7a8960a1a3e881dc1dba7a5a9f
Franco Catena <francocatena@gmail.com>
2014-03-06 09:31:52 -0300

    Add slim input

---
+++ app/inputs/slim_input.rb
@@ -0,0 +1,22 @@
+class SlimInput < SimpleForm::Inputs::Base
+  def input
+    @builder.text_field attribute_name, input_options
+  end
+
+  def label
+    false
+  end
+
+  def input_html_classes
+    super << 'form-control'
+  end
+
+  private
+
+    def input_options
+      {
+        placeholder: raw_label_text,
+        title: raw_label_text,
+      }.deep_merge input_html_options
+    end
+end
