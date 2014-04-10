a851b5ebf6294ddc0347b779dec6093c60124ee0
Franco Catena <francocatena@gmail.com>
2014-04-10 20:06:14 -0300

    Pagination helper test working

--- test/helpers/pagination_helper_test.rb
+++ test/helpers/pagination_helper_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class PaginationHelperTest < ActionView::TestCase
+  include WillPaginate::ViewHelpers
+
   test 'pagination' do
-    skip
-    assert_not_nil paginate(User.paginate(page: 1, per_page: 1))
+    mock_users_controller
+
+    users = User.paginate page: 1, per_page: 1
+
+    assert_not_nil paginate(users)
   end
+
+  private
+
+    def mock_users_controller
+      @controller = UsersController.new
+      @controller.request = ActionDispatch::TestRequest.new
+      @controller.request.action = 'index'
+
+      def @controller.default_url_options
+        { controller: 'users' }
+      end
+    end
 end
