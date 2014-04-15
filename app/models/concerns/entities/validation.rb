17be3c0354b7ab5ece2fee3002b7ee88dbdec6ed
Franco Catena <francocatena@gmail.com>
2014-04-15 09:55:59 -0300

    Better email check in users and entities

--- app/models/concerns/entities/validation.rb
+++ app/models/concerns/entities/validation.rb
@@ -6,6 +6,8 @@ module Entities::Validation
     validates :tax_id, presence: true, if: :validate_tax_id_presence?
     validates :tax_id, :tax_condition, :name, length: { maximum: 255 }
     validates :tax_id, :name, uniqueness: { case_sensitive: false, scope: :account_id }, allow_blank: true
-    validates :email, format: { with: EMAIL_REGEX }, allow_blank: true
+    validates :email,
+      uniqueness: { case_sensitive: false },
+      format: { with: EMAIL_REGEX }, allow_blank: true
   end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
 COOKIES_DOMAIN = ".#{ENV['APP_HOST'].sub /:.*/, ''}"
 RESERVED_SUBDOMAINS = ['admin', 'avatars', 'login', 'www']
-EMAIL_REGEX = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/i
+EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}\Z/i
