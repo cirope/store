dc8a843d89dae5a8077309a2aa8d94ea9a41b3a9
Franco Catena <francocatena@gmail.com>
2014-05-06 15:22:49 -0300

    ReceiptCommoditiesHelper test covered...

--- test/helpers/receipt_commodities_helper_test.rb
+++ test/helpers/receipt_commodities_helper_test.rb
@@ -2,6 +2,24 @@ require 'test_helper'
 
 class ReceiptCommoditiesHelperTest < ActionView::TestCase
   test 'receipt commodity bounds' do
-    skip
+    simple_fields_for ReceiptCommodity.new do |f|
+      bounds = receipt_commodity_bounds f
+
+      assert_equal 1, bounds.size
+      assert bounds.all?(&:new_record?)
+    end
+
+    simple_fields_for receipt_commodities(:one_delivery) do |f|
+      bounds = receipt_commodity_bounds f
+
+      assert_equal 1, bounds.size
+      assert bounds.all?(&:persisted?)
+    end
+  end
+
+  test 'link to bounds' do
+    simple_fields_for ReceiptCommodity.new do |f|
+      assert_match /href/, link_to_bounds(f)
+    end
   end
 end
