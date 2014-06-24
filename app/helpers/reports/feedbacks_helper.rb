5ec1c37304d172326b32a16913650079bdd772cc
Franco Catena <francocatena@gmail.com>
2014-06-24 11:53:46 -0300

    Add link to feedback index from report

--- app/helpers/reports/feedbacks_helper.rb
+++ app/helpers/reports/feedbacks_helper.rb
@@ -1,2 +1,7 @@
 module Reports::FeedbacksHelper
+  def feedback_count score
+    count = @feedback_stats[score] || 0
+
+    link_to_if count > 0, count, feedbacks_path(score: score)
+  end
 end
--- test/helpers/reports/feedbacks_helper_test.rb
+++ test/helpers/reports/feedbacks_helper_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class Reports::FeedbacksHelperTest < ActionView::TestCase
+  test 'feedback count' do
+    @feedback_stats = { 'good' => 1 }
+
+    assert_match    /href/, feedback_count('good')
+    assert_no_match /href/, feedback_count('poor')
+  end
 end
