8e5b9b095bd1c8a4b5bef301ffb1a7e4e9153714
Franco Catena <francocatena@gmail.com>
2014-12-14 20:58:44 -0300

    Ruby 2.1.5 and new Capistrano settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,12 +15,12 @@ preload_app true
 # "current" directory that Capistrano sets up.
 working_directory app_path
 
-listen '/tmp/unicorn.sock', backlog: 1024
+listen '/tmp/unicorn.login.sock', backlog: 1024
 
 # nuke workers after 60 seconds
 timeout 60
 
-pid '/tmp/unicorn.pid'
+pid '/tmp/unicorn.login.pid'
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
