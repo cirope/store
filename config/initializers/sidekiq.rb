f35add27f135ccd4837cb85dec401306c6262d20
Franco Catena <francocatena@gmail.com>
2016-01-03 18:53:38 -0300

    Remove redis namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/10', namespace: 'store' }
+  config.redis = { url: 'redis://localhost:6379/2' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/10', namespace: 'store' }
+  config.redis = { url: 'redis://localhost:6379/2' }
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -81,6 +81,6 @@ after_fork do |server, worker|
   # between any number of forked children (assuming your kernel
   # correctly implements pread()/pwrite() system calls)
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/10', size: 1, namespace: 'store' }
+    config.redis = { url: 'redis://localhost:6379/2', size: 1 }
   end
 end
